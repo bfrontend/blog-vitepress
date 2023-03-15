215cf791a839fb61c4cbf0ca09299c4ac39ecf7d
webxmsj <webxmsj@163.com>
2023-03-15 16:48:05 +0800

    🐞 fix:  错误修复

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -5,6 +5,7 @@ import themeConfig from '../config/theme'
 export default defineConfig({
   title: "webxmsj's blog",
   description: "Become a front-end developer",
+  base: '/blog/',
   lastUpdated: true,
   locales: {
     root: {
